d0317ed1fca632341987e2ed0c1b0397b9f3a2f5
langares <langares@yahoo.es>
2023-04-08 21:19:34 +0200

    Update fea.c

--- fea.c
+++ fea.c
@@ -2,5 +2,5 @@
 
 const DarkenAPI_System  *const $s = & ( DarkenAPI_System  ) { deSystem_init,  deSystem_update,  deSystem_end,   deSystem_add };
 const DarkenAPI_Manager *const $m = & ( DarkenAPI_Manager ) { deManager_init, deManager_update, deManager_end   };
-const DarkenAPI_Entity  *const $e = & ( DarkenAPI_Entity  ) { deEntity_new,   deEntity_update,  deEntity_change, deEntity_jump, deEntity_delete };
+const DarkenAPI_Entity  *const $e = & ( DarkenAPI_Entity  ) { deEntity_new,   deEntity_update,  deEntity_change, deEntity_set, deEntity_delete };
 const DarkenAPI_fea     *const $  = & ( DarkenAPI_fea     ) { darken, darken_end };
